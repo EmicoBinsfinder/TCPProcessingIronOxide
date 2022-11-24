8a5d97950ed2ba872eb2c910dfb6a52094d29d41
EmicoBinsfinder <109655670+EmicoBinsfinder@users.noreply.github.com>
2022-11-24 15:24:36 +0000

    Create Dissociation_Rates.py

---
+++ Dissociation_Rates.py
@@ -0,0 +1,24 @@
+Dissociation_Rate_400K_1GPa_Coefficients = [-0.5108, -0.5145, -0.507]
+Dissociation_Rate_400K_2GPa_Coefficients = [-0.841, -0.8527, -0.8435]
+Dissociation_Rate_400K_3GPa_Coefficients = [-1.352, -1.358, -1.345]
+Dissociation_Rate_400K_4GPa_Coefficients = [-1.483, -1.502, -1.465]
+Dissociation_Rate_400K_5GPa_Coefficients = [-1.519,  -1.543, -1.496]
+
+Dissociation_Rate_500K_1GPa_Coefficients = [-0.5568, -0.5656, -0.5479]
+Dissociation_Rate_500K_2GPa_Coefficients = [-0.7727,  -0.7839, -0.7615]
+Dissociation_Rate_500K_3GPa_Coefficients = [-1.46,  -1.474, -1.446]
+Dissociation_Rate_500K_4GPa_Coefficients = [-2.002, -2.035, -1.969]
+Dissociation_Rate_500K_5GPa_Coefficients = [-2.68, -2.702, -2.658]
+
+Dissociation_Rate_600K_1GPa_Coefficients = [-0.7349, -0.741, -0.7287]
+Dissociation_Rate_600K_2GPa_Coefficients = [-1.183, -1.194, -1.173]
+Dissociation_Rate_600K_3GPa_Coefficients = [-2.071, -2.093, -2.048]
+Dissociation_Rate_600K_4GPa_Coefficients = [-2.333, -2.351, -2.316]
+Dissociation_Rate_600K_5GPa_Coefficients = [-3.279, -3.306, -3.252]
+
+Dissociation_Rate_700K_1GPa_Coefficients = [-1.21, -1.215, -1.205]
+Dissociation_Rate_700K_2GPa_Coefficients = [-1.534, -1.55, -1.58]
+Dissociation_Rate_700K_3GPa_Coefficients = [-2.485, -2.53, -2.439]
+Dissociation_Rate_700K_4GPa_Coefficients = [-3.727, -3.779, -3.676]
+Dissociation_Rate_700K_5GPa_Coefficients = [-3.978, -4.026, -3.93]
+
